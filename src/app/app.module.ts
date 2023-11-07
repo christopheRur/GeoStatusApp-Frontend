544af69f52345daa5f87ab6cb089095221e0ba64
Rurangwa <chrisruu@yahoo.com>
2023-11-07 14:13:46 +0800

    added style and design

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { GeoStatComponent } from './components/geo-stat/geo-stat.component';
 
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
 @NgModule({
   declarations: [
     AppComponent,
     GeoStatComponent
   ],
   imports: [
-    BrowserModule
+
+    BrowserModule,HttpClientModule,FormsModule,
+    RouterModule.forRoot([{path:'geo-stat',
+    component: GeoStatComponent},
+
+  ])
   ],
+
   providers: [],
   bootstrap: [AppComponent]
 })
