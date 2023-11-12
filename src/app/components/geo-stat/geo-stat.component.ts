b51c483f96a996ac3a32d2ebbda4925cfcbb5761
Rurangwa <chrisruu@yahoo.com>
2023-11-12 13:03:59 +0800

    latest updates

--- src/app/components/geo-stat/geo-stat.component.ts
+++ src/app/components/geo-stat/geo-stat.component.ts
@@ -11,6 +11,10 @@ import { GeoService } from 'src/app/services/geo.service';
 export class GeoStatComponent implements OnInit {
 
   constructor(private geoServ:GeoService) { }
+
+viewBlock:boolean=false;
+
+
   asn=geoConstants.ASN;
   city=geoConstants.CITY;
   continent_code=geoConstants.CONTINENT_CODE;
@@ -39,9 +43,45 @@ export class GeoStatComponent implements OnInit {
   utc_offset=geoConstants.UTC_OFFSET;
   version=geoConstants.VERSION
 
+  asnRes?:string;
+  cityRes?:string;
+  continent_codeRes?:string
+  countryRes?:string;
+  country_areaRes?:number;
+  country_calling_codeRes?:string;
+  country_capitalRes?:string;
+  country_codeRes?:string;
+  country_code_iso3Res?:string;
+  country_nameRes?:string;
+  country_populationRes?:number;
+  country_tldRes?:string;
+  currencyRes?:string;
+  currency_nameRes?:string;
+  in_euRes?:boolean;
+  ipRes?:string;
+  languagesRes?:string;
+  latitudeRes?:number;
+  longitudeRes?:number;
+  networkRes?:string;
+  orgRes?:string;
+  postalRes?:string;
+  regionRes?:string;
+  region_codeRes?:string;
+  timezoneRes?:string;
+  utc_offsetRes?:string;
+  versionRes?:string;
+
 
   ngOnInit(): void {
-   // this.retrieveGeoStatusInformation();
+
+  }
+
+/**
+ * Display block of status
+ */
+  public displayBlock(){
+    this.retrieveGeoStatusInformation();
+    this.viewBlock=!this.viewBlock;
   }
 
 /**
@@ -52,11 +92,44 @@ export class GeoStatComponent implements OnInit {
 this.geoServ.getGeoStatus().subscribe(
   (response:geo)=>{
 
-console.log("------------------=-==-=-=-=-=-=--=-=-==->"+response.country_calling_code);
-
+this.setAllStatusValues(response);
 
   }
 )
+
   }
+
+private setAllStatusValues(response:geo):void{
+
+  this.asnRes=response.asn;
+  this.cityRes=response.city;
+  this.continent_codeRes=response.continent_code;
+  this.countryRes=response.country;
+  this.country_areaRes=response.country_area;
+  this.country_calling_codeRes=response.country_calling_code;
+  this.country_capitalRes=response.country_capital;
+  this.country_codeRes=response.country_code;
+  this.country_code_iso3Res=response.country_code_iso3;
+  this.country_nameRes=response.country_name;
+  this.country_populationRes=response.country_population;
+  this.country_tldRes=response.country_tld;
+  this.currencyRes=response.currency;
+  this.currency_nameRes=response.currency_name;
+  this.in_euRes=response.in_eu;
+  this.ipRes=response.ip;
+  this.languagesRes=response.languages;
+  this.latitudeRes=response.latitude;
+  this.longitudeRes=response.longitude;
+  this.networkRes=response.network;
+  this.orgRes=response.org;
+  this.postalRes=response.postal;
+  this.regionRes=response.region;
+  this.region_codeRes=response.region_code;
+  this.timezoneRes=response.timezone;
+  this.utc_offsetRes=response.utc_offset;
+  this.versionRes=response.version;
+
+
+}
 
 }
